b6ed5c98dd8e1b7608fde0626e9926d7ffd02eb5
JuliaDuF <70958643+JuliaDuF@users.noreply.github.com>
2026-08-08 15:09:17 -0700

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-javascript
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import HalfYear from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HalfYear />);
