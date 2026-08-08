d9d333627403579af7775e7a1f7d5f791ec4cd87
JuliaDuF <70958643+JuliaDuF@users.noreply.github.com>
2026-08-08 14:46:28 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+javascript
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HalfYear from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HalfYear />);
